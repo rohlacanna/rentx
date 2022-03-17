606c8ea2f4fc571d8633acda012f572e95b7fbd8
Rômulo Silva <romulo_lss@hotmail.com>
2022-03-17 19:33:42 -0300

    Fix: Fixing the imports

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,5 +1,8 @@
-import { Category } from "../model/Category";
-import { ICategoryRepository, ICreateCategoryDTO } from "./ICategoryRepository";
+import { Category } from "../../model/Category";
+import {
+  ICategoryRepository,
+  ICreateCategoryDTO,
+} from "../ICategoryRepository";
 
 class CategoriesRepository implements ICategoryRepository {
   private categories: Category[];
