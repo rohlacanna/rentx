83339c14270abdb382a4aa1f46c80f57210bd22c
Rômulo Silva <romulo_lss@hotmail.com>
2022-03-17 15:57:06 -0300

    feat: Creating specification repository

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,7 +1,25 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute() {
-    // eslint-disable-next-line no-console
-    console.log("TODO");
+  // eslint-disable-next-line prettier/prettier
+  constructor(private specificationsRepository: ISpecificationsRepository) { }
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists =
+      this.specificationsRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification already exists!");
+    }
+
+    this.specificationsRepository.create({
+      name,
+      description,
+    });
   }
 }
 
