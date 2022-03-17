18c9b4696616f2728c109b9d308fcf088f5ef22a
Rômulo Silva <romulo_lss@hotmail.com>
2022-03-17 19:22:26 -0300

    refactor: Refactoring the category listing

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -0,0 +1,15 @@
+import { Category } from "../../model/Category";
+import { ICategoryRepository } from "../../repositories/ICategoryRepository";
+
+class ListCategoriesUseCase {
+  // eslint-disable-next-line prettier/prettier
+  constructor(private categoriesRepository: ICategoryRepository) { }
+
+  execute(): Category[] {
+    const categories = this.categoriesRepository.list();
+
+    return categories;
+  }
+}
+
+export { ListCategoriesUseCase };
