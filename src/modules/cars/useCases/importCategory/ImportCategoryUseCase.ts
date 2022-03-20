791eeaa6edfdfe3b1e082e98b818a3d53e74534e
Rômulo Silva <romulo_lss@hotmail.com>
2022-03-20 16:55:56 -0300

    feat: Adding the stream concept

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,8 +1,18 @@
+import { parse as csvParse } from "csv-parse";
+import fs from "fs";
+
 class ImportCategoryUseCase {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  execute(file: any) {
-    // eslint-disable-next-line no-console
-    console.log(file);
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      // eslint-disable-next-line no-console
+      console.log(line);
+    });
   }
 }
 
