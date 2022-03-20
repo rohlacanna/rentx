a09e6618d2127a1525a2c557d6b0d1b04859ac8a
Rômulo Silva <romulo_lss@hotmail.com>
2022-03-20 16:40:33 -0300

    feat: Creating the use case to import categories

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,9 @@
+class ImportCategoryUseCase {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  execute(file: any) {
+    // eslint-disable-next-line no-console
+    console.log(file);
+  }
+}
+
+export { ImportCategoryUseCase };
