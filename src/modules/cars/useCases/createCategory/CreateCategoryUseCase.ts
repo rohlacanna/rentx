a5203941e346f45177d6d5d734d61e9f45ef8306
Rômulo Silva <romulo_lss@hotmail.com>
2022-03-21 17:45:32 -0300

    refactor: Refactoring create category with tsyringe

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,3 +1,7 @@
+/* eslint-disable prettier/prettier */
+
+import { inject, injectable } from "tsyringe";
+
 import { ICategoryRepository } from "../../repositories/ICategoryRepository";
 
 interface IRequest {
@@ -5,9 +9,12 @@ interface IRequest {
   description: string;
 }
 
+@injectable()
 class CreateCategoryUseCase {
-  // eslint-disable-next-line prettier/prettier
-  constructor(private categoriesRepository: ICategoryRepository) { }
+  constructor(
+    @inject("CategoriesRepository")
+    private categoriesRepository: ICategoryRepository,
+  ) { }
 
   async execute({ description, name }: IRequest): Promise<void> {
     const categoryAlreadyExists = await this.categoriesRepository.findByName(
