cc9dc8d2c096516a918252f1cad295f12d4ce6f4
Rômulo Silva <romulo_lss@hotmail.com>
2022-03-29 15:23:06 -0300

    feat: Verification of registered user email

--- src/modules/accounts/userCase/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/userCase/createUser/CreateUserUseCase.ts
@@ -20,6 +20,12 @@ class CreateUserUseCase {
     driver_license,
   }: ICreateUserDTO): Promise<void> {
 
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists!")
+    }
+
     const passwordHash = await hash(password, 8);
 
     await this.usersRepository.create({
