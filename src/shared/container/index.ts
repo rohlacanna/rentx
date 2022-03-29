6fb7708f1ba7d0092b6ef5030cf091130fc0320d
Rômulo Silva <romulo_lss@hotmail.com>
2022-03-29 14:36:02 -0300

    feat: Creating user repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from "tsyringe";
 
+import { UsersRepository } from "../../modules/accounts/implementations/UsersRepository";
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
 import { ICategoryRepository } from "../../modules/cars/repositories/ICategoryRepository";
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
 import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
@@ -13,4 +15,9 @@ container.registerSingleton<ICategoryRepository>(
 container.registerSingleton<ISpecificationsRepository>(
   "SpecificationsRepository",
   SpecificationsRepository,
+);
+
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository",
+  UsersRepository,
 );
