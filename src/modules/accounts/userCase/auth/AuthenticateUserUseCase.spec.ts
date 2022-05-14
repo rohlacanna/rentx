29840cdba218495b9b03c9a4f953b75f8cec580f
Rômulo Silva <romulo_lss@hotmail.com>
2022-05-14 13:03:19 -0300

    fix: application imports

--- src/modules/accounts/userCase/auth/AuthenticateUserUseCase.spec.ts
+++ src/modules/accounts/userCase/auth/AuthenticateUserUseCase.spec.ts
@@ -1,6 +1,7 @@
-import { AppErros } from "../../../../errors/AppErrors";
-import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
-import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory";
+import { AppErros } from "@errors/AppErrors";
+import { ICreateUserDTO } from "@modules/accounts/dtos/ICreateUserDTO";
+import { UsersRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersRepositoryInMemory";
+
 import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
 import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
 
--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,5 +1,6 @@
-import { AppErros } from "../../../../errors/AppErrors";
-import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { AppErros } from "@errors/AppErrors";
+import { CategoriesRepositoryInMemory } from "@modules/cars/repositories/in-memory/CategoriesRepositoryInMemory";
+
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
 let createCategoryUseCase: CreateCategoryUseCase;
