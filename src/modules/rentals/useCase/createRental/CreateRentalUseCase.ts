ffa5f42b4703ea78b880d590bb1bc9a99ba45fa8
Rômulo Silva <romulo_lss@hotmail.com>
2022-08-07 15:42:59 -0300

    feat: Create test for the rental registration

---
+++ src/modules/rentals/useCase/createRental/CreateRentalUseCase.ts
@@ -0,0 +1,47 @@
+/* eslint-disable prettier/prettier */
+
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { AppErros } from "@shared/errors/AppErrors";
+
+interface IRequest {
+  user_id: string;
+  car_id: string;
+  expected_return_date: Date;
+}
+
+class CreateRentalUseCase {
+  constructor(private rentalsRepository: IRentalsRepository) { }
+
+  async execute({
+    user_id,
+    car_id,
+    expected_return_date,
+  }: IRequest): Promise<Rental> {
+    const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
+      car_id,
+    );
+
+    if (carUnavailable) {
+      throw new AppErros("Car is unavailable!");
+    }
+
+    const rentalOpenToUser = await this.rentalsRepository.findOpenRentalByUser(
+      user_id,
+    );
+
+    if (rentalOpenToUser) {
+      throw new AppErros("There's a rental is progress for user!");
+    }
+
+    const rental = await this.rentalsRepository.create({
+      user_id,
+      car_id,
+      expected_return_date,
+    });
+
+    return rental;
+  }
+}
+
+export { CreateRentalUseCase };
