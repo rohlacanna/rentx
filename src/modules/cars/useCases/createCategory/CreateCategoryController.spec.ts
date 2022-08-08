a8343b721327722fb0550e97e5888add51f427c5
Rômulo Silva <romulo_lss@hotmail.com>
2022-08-08 09:46:36 -0300

    WIP

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,43 @@
+import { hash } from "bcrypt";
+import request from "supertest";
+import { Connection } from "typeorm";
+import { v4 as uuid } from "uuid";
+
+import { app } from "@shared/infra/http/app";
+import createConnection from "@shared/infra/typeorm";
+
+let connection: Connection;
+describe("Create Category Controller", async () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = uuid();
+    const password = await hash("admin", 8);
+
+    await connection.query(
+      `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
+      values('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXXX')`,
+    );
+  });
+
+  afterAll(async () => {
+    // await connection.dropDatabase();
+    await connection.close;
+  });
+
+  it("should be able to create a new category", async () => {
+    const responseToken = await request(app).post("/sessions").send({
+      email: "admin@rentx.com.br",
+      password: "admin",
+    });
+
+    console.log(responseToken.body);
+
+    const response = await request(app).get("/categories").send({
+      name: "Category Supertest",
+      description: "Category Supertest",
+    });
+    expect(response.status).toBe(201);
+  });
+});
